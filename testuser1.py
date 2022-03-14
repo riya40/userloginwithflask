22dad9fd78dc24613de9d5bd7d9a320b5e88582a
riya40 <priyanunna20@gmail.com>
2022-03-14 14:54:29 +0530

    Added:User Login And Access the Token

--- testuser1.py
+++ testuser1.py
@@ -1,7 +1,10 @@
 from flask import jsonify
 from flask import request
 from flask import Flask
-
+from flask_mail import Mail, Message
+from flask_jwt_extended import create_access_token
+from flask_jwt_extended import get_jwt_identity
+from flask_jwt_extended import jwt_required
 from flask_jwt_extended import JWTManager
 
 from pymongo import MongoClient
@@ -31,6 +34,27 @@ def register():
         user_info = dict(firstname=firstname, lastname=lastname, email=email, password=password)
         user.insert_one(user_info)
         return jsonify(message="user added successfull")
+
+
+@app.route("/login", methods=["POST"])
+def login():
+    """
+    it returns the user is already  registered and checks the username and password
+    if username and password matches it generates the token for access
+    """
+    if request.is_json:
+        email = request.json.get["email"]
+        password = request.json.get["password"]
+    else:
+        email = request.form["email"]
+        password = request.form["password"]
+
+    test = user.find_one({"email": email, "password": password})
+    if test:
+        access_token = create_access_token(identity=email)
+        return jsonify(message="Login Succeeded!", access_token=access_token), 201
+    else:
+        return jsonify(message="Bad Email or Password"), 401
 
 
 if __name__ == "__main__":
